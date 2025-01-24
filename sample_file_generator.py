6ee28eedb993a321b7aca5cf13a56988a1cb9421
ka1sser <serenokaisser.dev@gmail.com>
2025-01-24 17:06:03 +0800

    Added docstring per function. create_sample_recharge_file() dynamic naming to be fixed.

--- sample_file_generator.py
+++ sample_file_generator.py
@@ -4,6 +4,13 @@ import string
 from datetime import datetime, timedelta
 
 def generate_msidn():
+    """
+    This function will generate a random msidn
+    
+    Returns:
+        msidn (str): This will be used as a data entry for the .csv file.
+    """
+    
     num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     code = 971
     random_numbers = random.choices(num, k=9)
@@ -15,10 +22,30 @@ def generate_msidn():
     return msidn
 
 def generate_event_date_time():
-    now = datetime.now().strftime("%d%m%Y%H%M")
+    """
+    This function will generate a random date from current date to 30 days ago.
+    
+    Returns:
+        now (str): This will be used as a data entry for the .csv file. Date follows
+        the format: DaysMonthYearHourMinute (DDMMYYYYHHMM)
+    """
+    
+    end = datetime.now()
+    start = end - timedelta(days=30)
+    
+    random_date = start + timedelta(seconds=random.randint(0, int((end - start).total_seconds())))
+    now = random_date.strftime("%d%m%Y%H%M")
+    
     return now
 
 def generate_service_class():
+    """
+    This function generate a three digit number which serves as the service class.
+
+    Returns:
+        service_class (str): This will be used as a data entry for the .csv file.
+    """
+    
     first_digit_choices = [0, 1, 2, 3, 4, 5]
     second_digit_choices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     third_digit_choices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -32,6 +59,15 @@ def generate_service_class():
     return service_class
 
 def generate_recharge_amt():
+    """
+    This function will generate a combination of numbers depending on the
+    output of num_of_digits which decides how many digits the number
+    will have.
+
+    Returns:
+        recharge_amt (str): This will be used as a data entry for the .csv file.
+    """
+    
     digits = [2, 3, 4, 5]
     num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     first_digit = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -50,18 +86,41 @@ def generate_recharge_amt():
     return recharge_amt
 
 def generate_payment_method():
+    """
+    This function will generate a random code with a prefix of '0'
+
+    Returns:
+        payment_method (str): This will be used as a data entry for the .csv file.
+    """
+    
     num = [1, 2, 3]
     payment_method = "0" + str(random.choice(num))
     
     return payment_method
     
 def generate_subscriber_category():
+    """
+    This function will generate a random category based on the
+    categories list.
+
+    Returns:
+        subscriber_category (str): This will be used as a data entry for the .csv file.
+    """
+    
     categories = ["YTH", "STD", "BSC", "SPL"]
     subscriber_category = random.choice(categories)
     
     return subscriber_category
 
 def generate_location():
+    """
+    This function will generate a random location based on the
+    locations list.
+
+    Returns:
+        location (str): This will be used as a data entry for the .csv file.
+    """
+    
     locations = ["X10", "X11", "X12", "X13"]
     location = random.choice(locations)
     
@@ -69,19 +128,44 @@ def generate_location():
             
 def generate_content_for_file(file_path, msidn, event_date_time, service_class,
                               recharge_amt, payment_method, subscriber_category, location):
+    """
+    This function will generate the content of the file to be created per line.
+
+    Args:
+        file_path (dir): File where the content will be generated.
+        msidn (str): Randomly generated msidn data
+        event_date_time (str): Randomly generated date and time data
+        service_class (str): Randomly generated service class data
+        recharge_amt (str): Randomly generated recharge amount data
+        payment_method (str): Randomly generated payment method data
+        subscriber_category (str): Randomly generated subscriber category data
+        location (str): Randomly generated location data
+    """
     
     with open(file_path, "a") as file:
-        content = f"{msidn},{event_date_time},{service_class},{recharge_amt},{payment_method},{subscriber_category},{location}\n"
+        content = f"{msidn},10,{event_date_time},{service_class},{recharge_amt},{payment_method},{subscriber_category},{location}\n"
         
         file.write(content)
 
 def create_sample_recharge_file(base_path, num_of_lines):
-    
+    """
+    This function will generate the file with contents generated randomly by different
+    functions and compiled by running the generate_content_for_file() function.
+
+    Args:
+        base_path (dir): Directory where the file will be generated
+        num_of_lines (int): Number of lines of content to be generated
+    """
     os.makedirs(base_path, exist_ok=True)
     
-    file_name = f"EvenFile_Recharge_000.csv"
+    date = datetime.now().strftime("%d%m%Y%_H%M")
+    
+    file_name = f"EvenFile_Recharge_{unique_num}_{date}.csv"
     file_path = os.path.join(base_path, file_name)
 
+    with open(file_path, "w") as f:
+        f.write("MSIDN,EventType,EventDateAndTime,ServiceClass,RechargeAmount,PaymentMethod,Category,Location\n")
+    
     x=0
     while x < num_of_lines:
         msidn = generate_msidn()
