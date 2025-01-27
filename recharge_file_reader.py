704f9321ba9249aa4947e931ccdee26649b6043e
ka1sser <serenokaisser.dev@gmail.com>
2025-01-27 13:34:48 +0800

    Functionality for matched csv files to be combined in one big dataframe done. Fixed some functions as well.

--- recharge_file_reader.py
+++ recharge_file_reader.py
@@ -1,4 +1,5 @@
 """
+
 This script will read through all the directories included in the input path.
 
 It shall:
@@ -7,7 +8,9 @@ It shall:
     a. Report 1: Location wise total recharge amount for a complete day
     b. Report 2: Category-wise total recharge amount for a complete day
     b. Report 3: Total cash, card, and payment.
+    
 """
+
 import os
 import tomli
 import pandas as pd
@@ -72,17 +75,35 @@ def combined_files_per_dir(input_path):
     This function will check the files in each directory and compile it in one list.
 
     Returns:
-        files (list): List containing all the files within the sub directories
+        files_with_path (list): List containing all the files within the sub directories
     """
     
     sub_dirs = get_sub_dir(input_path)
     
-    files = []
+    files_with_path = []
     for sub_dir in sub_dirs:
         files_in_sub_dir = os.listdir(sub_dir)
-        files = files + files_in_sub_dir
+        for file in files_in_sub_dir:
+            file_path = os.path.abspath(os.path.join(sub_dir, file))
+            files_with_path.append(file_path)
+            
+    return files_with_path
 
-    return files
+def get_base_name(files):
+    """
+    This function gets the basename of the file path
+
+    Args:
+        files (list): List of file paths
+
+    Returns:
+        list_of_basename (list): List of strings of basename
+    """
+    list_of_basename = []
+    for file in files:
+        list_of_basename.append(os.path.basename(file))
+        
+    return list_of_basename
 
 def check_filename(files):
     
@@ -95,8 +116,7 @@ def check_filename(files):
                       to be checked.
 
     Returns:
-        matched_files (list): Returns a list of matching file names. To be os.path.joined() to
-        the original path for it to be accessed properly.
+        matched_files (list): Returns a list of matching file names
     """
     
     current_date = datetime.now()
@@ -105,28 +125,77 @@ def check_filename(files):
     matched_files = []
     for file in files:
         if file.find(formatted_date) != -1:
-            matched_files.append(file)
+            if file.endswith('.csv'):
+                matched_files.append(file)
     
     return matched_files
+
+def get_csv_files_to_read(input_path):
+    """
+    This function will get the csv files in different directories that matches the current date.
+
+    Args:
+        input_path (dir): Directory of the input path
+
+    Returns:
+        csv_files_to_read (list): Returns a list containing the file path of the csv files
+    """
     
+    files_in_path = combined_files_per_dir(input_path)
+    filenames = get_base_name(files_in_path)
+    matched_files = check_filename(filenames)
+
+    csv_files_to_read = []
+    for file in matched_files:
+        for file_path in files_in_path:
+            if file_path.find(file) != -1:
+                csv_files_to_read.append(file_path)
+                
+    return csv_files_to_read
+   
 def read_csv(file):
+    """
+    This function reads and loads the csv file.
+
+    Args:
+        file (str): This input should be the file path for the
+
+    Returns:
+        csv (pandas.core.frame.DataFrame): Returns the csv via pandas dataframe
+    """
     
     csv = pd.read_csv(file)
     
     return csv
 
+def combine_matched_csv(files):
+    """
+    This function will combine all the csv files data in one big df
+
+    Args:
+        files (list): List of csv file path that matches the current date
+
+    Returns:
+        combined_df (pandas.core.frame.DataFrame): Returns a data frame with combined data from all the listed csv files
+    """
+    csv_list = []
+    for file in files:
+        df = read_csv(file)
+        csv_list.append(df)
+        
+    combined_df = pd.concat(csv_list, ignore_index=True)
+    
+    return combined_df
+
 def main():
     
     config = import_config_file()
     input_path = import_file_path(config)
-    files = (combined_files_per_dir(input_path))
     
-    matched_files = check_filename(files)
-    #file = "C:/Users/eserkai/OneDrive - Ericsson/Documents/Programming/Python/Training/recharge-file-handling/sample_data/subdir_1/EventFile_Recharge_006_27012025_0926.csv"
-        
-    #csv = read_csv(file)
-    #print(csv.head(3))
-    #print(csv["Category"].head(5))
+    csv_files_to_read = get_csv_files_to_read(input_path)
+    combined_df = combine_matched_csv(csv_files_to_read)
+    
+    print(type(combined_df))
     
 if __name__ == "__main__":
     
