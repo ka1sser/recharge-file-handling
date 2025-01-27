1fe6622018676418823c874c5506cd40bfd0331c
ka1sser <serenokaisser.dev@gmail.com>
2025-01-27 09:06:07 +0800

    Initial setup for the recharge file reader

---
+++ recharge_file_reader.py
@@ -0,0 +1,16 @@
+"""
+This script will read through all the directories included in the input path.
+
+It shall:
+1. Create a log that has an output on when the file was picked up by the script with it's filename and log if any error persist during the pickup of the file.
+2. It should create multiple report files for a complete day and contain below details:
+    a. Report 1: Location wise total recharge amount for a complete day
+    b. Report 2: Category-wise total recharge amount for a complete day
+    b. Report 3: Total cash, card, and payment.
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    pass
